e6087a5aef2c7336b0135bc3b52d7d3df12a02ae
shahdivij <divijshah11@gmail.com>
2026-02-06 21:22:28 +0530

    api added for warrenty

--- src/api/warrenty/controllers/warrenty.js
+++ src/api/warrenty/controllers/warrenty.js
@@ -9,7 +9,20 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::warrenty.warrenty', ({ strapi }) => ({
   async create(ctx) {
     try {
-      const { SerialNumber, ...otherData } = ctx.request.body.data;
+      // Handle both JSON and multipart/form-data requests
+      let requestData;
+      
+      if (ctx.request.body.data) {
+        // If data is a string (from form-data), parse it
+        requestData = typeof ctx.request.body.data === 'string' 
+          ? JSON.parse(ctx.request.body.data) 
+          : ctx.request.body.data;
+      } else {
+        // If no data wrapper, use body directly
+        requestData = ctx.request.body;
+      }
+
+      const { SerialNumber } = requestData;
 
       // Validate that SerialNumber is provided
       if (!SerialNumber) {
@@ -33,6 +46,35 @@ module.exports = createCoreController('api::warrenty.warrenty', ({ strapi }) => 
       if (existingWarranty && existingWarranty.length > 0) {
         return ctx.badRequest('A warranty already exists for this serial number.');
       }
+
+      // Handle file upload if present
+      let uploadedFileId = null;
+      if (ctx.request.files && ctx.request.files['files.Invoice']) {
+        const file = ctx.request.files['files.Invoice'];
+        
+        // Upload the file using Strapi's upload service
+        const uploadedFiles = await strapi.plugins.upload.services.upload.upload({
+          data: {},
+          files: file,
+        });
+        
+        if (uploadedFiles && uploadedFiles.length > 0) {
+          uploadedFileId = uploadedFiles[0].id;
+        }
+      }
+
+      // Prepare data for creation
+      const dataToCreate = {
+        ...requestData
+      };
+
+      // Add the uploaded file ID if file was uploaded
+      if (uploadedFileId) {
+        dataToCreate.Invoice = uploadedFileId;
+      }
+
+      // Update ctx.request.body.data with the file ID
+      ctx.request.body.data = dataToCreate;
 
       // Create the warranty record using the default create method
       const response = await super.create(ctx);
--- src/api/warrenty/routes/warrenty.js
+++ src/api/warrenty/routes/warrenty.js
@@ -6,31 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-const defaultRouter = createCoreRouter('api::warrenty.warrenty');
-
-const customRouter = (innerRouter, extraRoutes = []) => {
-  let routes;
-  return {
-    get prefix() {
-      return innerRouter.prefix;
-    },
-    get routes() {
-      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
-      return routes;
-    },
-  };
-};
-
-const myExtraRoutes = [
-  {
-    method: 'POST',
-    path: '/warrenties',
-    handler: 'warrenty.create',
-    config: {
-      policies: [],
-      middlewares: [],
-    },
-  },
-];
-
-module.exports = customRouter(defaultRouter, myExtraRoutes);
+module.exports = createCoreRouter('api::warrenty.warrenty');
